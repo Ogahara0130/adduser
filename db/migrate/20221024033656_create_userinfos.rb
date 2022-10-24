702c5c925d4f46deecaa22c53ea42309746b604c
miyagi <e115725@ie.u-ryukyu.ac.jp>
2022-10-24 12:53:08 +0900

    UserinfoモデルとDBを作成

---
+++ db/migrate/20221024033656_create_userinfos.rb
@@ -0,0 +1,30 @@
+class CreateUserinfos < ActiveRecord::Migration[7.0]
+  def change
+    create_table :userinfos do |t|
+      t.string :name_first
+      t.string :name_last
+      t.string :name_first_read
+      t.string :name_last_read
+      t.string :sex
+      t.date :birthday
+      t.string :postnumber
+      t.string :prefecture
+      t.string :municipalities
+      t.string :street
+      t.string :tel_main
+      t.string :tel_selpfhone
+      t.string :mail_pc
+      t.string :mail_pass
+      t.string :survey_experience
+      t.string :desire_survey
+      t.string :nearest_office
+      t.string :possession_car
+      t.string :nearest_station
+      t.string :cognitive_pathway
+      t.string :cognitive_pathway_other
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,41 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_10_24_033656) do
+  create_table "userinfos", force: :cascade do |t|
+    t.string "name_first"
+    t.string "name_last"
+    t.string "name_first_read"
+    t.string "name_last_read"
+    t.string "sex"
+    t.date "birthday"
+    t.string "postnumber"
+    t.string "prefecture"
+    t.string "municipalities"
+    t.string "street"
+    t.string "tel_main"
+    t.string "tel_selpfhone"
+    t.string "mail_pc"
+    t.string "mail_pass"
+    t.string "survey_experience"
+    t.string "desire_survey"
+    t.string "nearest_office"
+    t.string "possession_car"
+    t.string "nearest_station"
+    t.string "cognitive_pathway"
+    t.string "cognitive_pathway_other"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end

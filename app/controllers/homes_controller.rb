33c72eddc18d25c6fdf5ea1497b4c5097cae7188
miyagi <e115725@ie.u-ryukyu.ac.jp>
2022-10-28 12:03:55 +0900

    希望する調査のチェックボックスをboolean型に修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,18 @@
 class HomesController < ApplicationController
   def top
+    @userinfos = Userinfo.new
   end
+
   def confirm
+    @userinfos = Userinfo.new(userinfos_params)
+  end 
+
+	def back
+		@userinfos = Userinfo.new(userinfos_params)
+		render :top
+	end
+
+  def userinfos_params
+    params.require(:userinfo).permit(:name_first, :name_last, :name_first_read, :name_last_read, :sex, :birthday, :postnumber, :prefecture, :municipalities, :street, :tel_main, :tel_selpfhone, :mail_pc, :mail_pass, :survey_experience, :traffic_survey, :driving_survey, :bus_survey, :questionnaire_distribution, :interview_survey, :visit_survey, :undercover_investigation, :office_staff, :nearest_office, :possession_car, :nearest_station, :cognitive_pathway, :cognitive_pathway_other, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  root to: 'homes#top'
+
   get 'top' => 'homes#top'
-  get 'confirm' => 'homes#confirm'
+  post 'back' => 'homes#back'
+  post 'confirm' => 'homes#confirm'
 end

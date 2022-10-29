45c5c0246516d253677b4fcf69507767a267bcb7
ogahara-0130 <ogahara.thinking.channel@gmail.com>
2022-10-29 13:17:19 +0900

    管理ユーザのログイン画面作成と反映

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
   get 'confirm' => 'homes#confirm'
+  get    '/login' => 'sessions#new'
+  post   '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+  resource :admin_user, only: [:new, :create, :show]
 end

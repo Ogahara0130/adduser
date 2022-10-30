84c504ff5281075ef232773fa66c8a48fa93b7e8
miyagi <e115725@ie.u-ryukyu.ac.jp>
2022-10-30 18:49:14 +0900

    トップ画面に必須を追加 メールアドレス、郵便番号のバリデーションを追加

--- app/models/userinfo.rb
+++ app/models/userinfo.rb
@@ -11,32 +11,51 @@ class Userinfo < ApplicationRecord
               presence: { message: 'が選択されていません。' } 
     #郵便番号
     validates :postnumber, presence: { message: 'が入力されていません。' } 
+    validates :postnumber, format: { with: /\A\d{3}[-]?\d{4}\z/, message: 'は半角英数字で正しい桁数で入力してください。'} 
     #都道府県
     validates :prefecture, presence: { message: 'が選択されていません。' } 
-    #市区町村名,番地名,自宅電話番号,携帯電話番号
+    #市区町村名,番地名
     validates :municipalities,
               :street,
-              :mail_pc,
-              :mail_pass,
               presence: { message: 'が入力されていません。' }
+
+    #PCメールアドレス,携帯メールアドレス          
+    validates :mail_pc_or_pass, presence: { message: 'を入力してください。' }
+    validates :mail_pc, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, if: :mail_pc_presence?
+    validates :mail_pass, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, if: :mail_pass_presence?
+
     #調査経験回数
     validates :survey_experience, presence: { message: 'が選択されていません。' } 
     #希望する調査
     validates :desire_survey_check, presence: { message: 'が選択されていません。' }  
-    #最寄SRC事務所,車の保有について
-    validates :nearest_office,
-              :possession_car,
+    #車の保有について,認知経路
+    validates :possession_car,
               :cognitive_pathway,
               presence: { message: 'が選択されていません。' } 
-    #自宅最寄り駅
-    validates :nearest_station, presence: { message: 'が入力されていません。' }
     #認知経路
     validates :cognitive_pathway, presence: { message: 'が選択されていません。' } 
-    #認知経路 その他,パスワード
-    validates :cognitive_pathway_other,
-              :password,
-              presence: { message: 'が入力されていません。' } 
-    
+    #パスワード
+    validates :password, presence: { message: 'が入力されていません。' } 
+ 
+    #PCメールアドレスか携帯メールアドレスのどちらかが入力されているか
+    private
+    def mail_pc_or_pass
+        mail_pc.presence or 
+        mail_pass.presence
+    end
+
+    #PCメールアドレスが入力されているか
+    private
+    def mail_pc_presence?
+        mail_pc.presence
+    end    
+
+    #携帯メールアドレスが入力されているか
+    private
+    def mail_pass_presence?
+        mail_pass.presence
+    end   
+
     #希望する調査のチェックボックスのどれかにチェックされているか
     private
     def desire_survey_check
